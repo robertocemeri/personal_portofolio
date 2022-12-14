ab6fe460d9a9436d6c1c27a47371de148220d495
Roberto Cemeri <robertocemeri29@gmail.com>
2022-12-14 20:03:26 +0100

    initial

---
+++ build/precache-manifest.21eecf131c52dfcefd0a515d7759f673.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a4b6cdad08c78bae9b0",
+    "url": "/static/css/main.a1f3c759.chunk.css"
+  },
+  {
+    "revision": "4a4b6cdad08c78bae9b0",
+    "url": "/static/js/main.4a4b6cda.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "25bf92172d7f8749c2b92299c24f2fc0",
+    "url": "/index.html"
+  }
+];
---
+++ src/resumeData.js
@@ -0,0 +1,108 @@
+let resumeData = {
+  imagebaseurl: "https://rbhatia46.github.io/",
+  name: "Roberto Cemeri",
+  role: "Full Stack Web Developer ",
+  linkedinId: "Your LinkedIn Id",
+  skypeid: "Your skypeid",
+  roleDescription:
+    "I am currently a pre-final year student at University of Tirana and pursuing my M.Information technology and communication degree. I am a self taught Full Stack Web Developer. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/robertocemeri/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "http://github.com/robertocemeri",
+      className: "fa fa-github",
+    },
+    {
+      name: "instagram",
+      url: "https://www.instagram.com/robertocemeri/",
+      className: "fa fa-instagram",
+    },
+  ],
+  aboutme:
+    "I am currently a pre-final year student at University of Tirana and pursuing my M.Information technology and communication degree. I am a self taught Full Stack Web Developer. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+  address: "Tirane, Albania",
+  website: "https://robertocemeri.com",
+  education: [
+    {
+      UniversityName: "The LNM Insitute of Information Technology",
+      specialization: "Some specialization",
+      MonthOfPassing: "Aug",
+      YearOfPassing: "2020",
+      Achievements: "Some Achievements",
+    },
+    {
+      UniversityName: "Some University",
+      specialization: "Some specialization",
+      MonthOfPassing: "Jan",
+      YearOfPassing: "2018",
+      Achievements: "Some Achievements",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Some Company",
+      specialization: "Some specialization",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2018",
+      Achievements: "Some Achievements",
+    },
+    {
+      CompanyName: "Some Company",
+      specialization: "Some specialization",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2018",
+      Achievements: "Some Achievements",
+    },
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "Reactjs",
+    },
+  ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project.jpg",
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.png",
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+  ],
+};
+
+export default resumeData;
